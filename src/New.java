0fda7568538efada6fe371b6ad6b46e707d4df2e
Jaspreet Singh <jaspreet.singh02@ad.infosys.com>
2021-01-04 20:36:04 +0530

    Modified New.java after conflict

--- src/New.java
+++ src/New.java
@@ -2,6 +2,7 @@ public class New{
 
 	public static void main(String[] args){
 		System.out.println("This is the new file created via Git");
+	
 		System.out.println("Commit from Branch2 only!");
 	}
 }
